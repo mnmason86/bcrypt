a32014e4cb0f2d75c58731e64a4f04d0e226008b
Mandy Mason <mnmason86@gmail.com>
2022-09-01 19:27:20 -0400

    initial setup

---
+++ src/test/java/com/mnmason6/bcrypt/BcryptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mnmason6.bcrypt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BcryptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
